5e3994a1eee46f875c18e69cb7fee19ea707b88d
werco.ir <info.werco@gmail.com>
2024-07-20 05:06:17 -0700

    route about added

--- blueprints/general.py
+++ blueprints/general.py
@@ -2,6 +2,10 @@ from flask import Blueprint
 app = Blueprint("general", __name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def main():  # put application's code here
     return 'werco.ir!'
+
+@app.route('/about')
+def about():  # put application's code here
+    return 'about us!'
 
