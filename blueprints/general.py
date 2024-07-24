b88382394ea7b22bd62f0ff1df6c8166284fdbf4
werco.ir <info.werco@gmail.com>
2024-07-24 06:25:14 -0700

    main and about added

--- blueprints/general.py
+++ blueprints/general.py
@@ -1,11 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from models.product import Product
+
 app = Blueprint("general", __name__)
 
 @app.route('/')
-def main():  # put application's code here
-    return 'werco.ir!'
+def main():
+    products = Product.query.all()
+    return render_template("main.html", products=products)
 
 @app.route('/about')
 def about():  # put application's code here
-    return 'about us!'
+    return render_template("about.html")
 
