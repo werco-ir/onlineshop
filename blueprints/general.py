7d5c2533587b80848aca5776c7b082bd2ec7e798
werco.ir <info.werco@gmail.com>
2024-08-07 02:24:10 -0700

    better css in product page

--- blueprints/general.py
+++ blueprints/general.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template,request
-
+from sqlalchemy.sql.expression import func
 from models.product import Product
 
 app = Blueprint("general", __name__)
@@ -21,7 +21,14 @@ def main():
 def product(id, name):
     product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(Product.active == 1).first_or_404()
 
-    return render_template('product.html', product=product)
+
+    another_products = Product.query.filter(Product.active==1).filter(Product.name.like(f'%{product.name[0:10]}%')).order_by(func.random()).limit(5).all()
+
+
+    return render_template('product.html', product=product , another_products=another_products)
+
+
+
 
 
 @app.route('/about')
